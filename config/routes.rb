db393ca2897ddc7bde5069b66b8e809470362b4c
App Academy Student <student@appacademy.io>
2015-10-13 12:01:14 -0700

    create JSON API for users and setup React

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#root'
+  get 'login', to: 'sessions#new'
+  get 'signup', to: 'users#new'
   resources :users, only: [:create, :new, :index]
   resource :session, only: [:create, :new, :destroy]
+
+  namespace :api, defaults: {format: :json} do
+    resources :users
+  end
 end
