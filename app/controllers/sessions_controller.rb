27d73d62c697015cb9d8827309b6409c31d11561
Akansh Murthy <ak45m16@gmail.com>
2015-10-23 09:38:28 -0700

    add error messaging for sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
     )
 
     if user.nil?
-      flash.now[:errors] = ["Incorrect username and/or password"]
+      flash[:errors] = ["Incorrect username and/or password"]
       redirect_to new_user_url
     else
       login_user!(user)
