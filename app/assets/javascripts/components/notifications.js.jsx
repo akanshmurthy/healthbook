ec42583655f21bb72624f22ce651743f12f54bce
Akansh Murthy <ak45m16@gmail.com>
2015-11-01 17:41:45 -0800

    shorten length of notifs bar

--- app/assets/javascripts/components/notifications.js.jsx
+++ app/assets/javascripts/components/notifications.js.jsx
@@ -43,14 +43,18 @@
     componentWillUnmount: function () {
       root.NotificationStore.removeChangeListener(this._onChange);
     },
+    delete: function(id) {
+      root.NotificationUtil.destroy(id)
+    },
     render: function () {
       var that = this;
       return (
         <ul className="dropdown-menu" aria-labelledby="dLabel">
-                         <ul className="list-group">
+                         <ul id="dropdownlist" className="list-group">
                            {that.state.notifications.reverse().map(function(el){
                              return <li key={el.id} className="list-group-item">{el.body}
                                  <h6>{jQuery.timeago(el.created_at)}</h6>
+                                 <a onClick={that.delete.bind(that, el.id)}>Delete</a>
                                </li>;
                            })}
                          </ul>
