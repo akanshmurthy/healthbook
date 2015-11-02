3ae119a605115156fb07abd0fcb2986ce8922d61
Akansh Murthy <ak45m16@gmail.com>
2015-11-01 21:56:56 -0800

    make search results container larger

--- app/assets/javascripts/components/notifications.js.jsx
+++ app/assets/javascripts/components/notifications.js.jsx
@@ -50,15 +50,15 @@
       var that = this;
       return (
         <ul className="dropdown-menu" aria-labelledby="dLabel">
-                         <ul id="dropdownlist" className="list-group">
-                           {that.state.notifications.reverse().map(function(el){
-                             return <li key={el.id} className="list-group-item">{el.body}
-                                 <h6>{jQuery.timeago(el.created_at)}</h6>
-                                 <a onClick={that.delete.bind(that, el.id)}>Delete</a>
-                               </li>;
-                           })}
-                         </ul>
-                      </ul>
+         <ul id="dropdownlist" className="list-group">
+           {that.state.notifications.reverse().map(function(el){
+             return <li key={el.id} className="list-group-item">{el.body}
+                 <h6>{jQuery.timeago(el.created_at)}</h6>
+                 <a onClick={that.delete.bind(that, el.id)}>Delete</a>
+               </li>;
+           })}
+         </ul>
+        </ul>
       );
     }
   });
